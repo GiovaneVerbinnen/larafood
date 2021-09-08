e29316cebd6fc787de13447cc335015e8619d3a7
Giovane Verbinnen de Oliveira <gioverbinnen@gmail.com>
2021-09-08 11:17:16 -0300

    initi commit

---
+++ resources/views/admin/pages/plans/index.blade.php
@@ -0,0 +1,75 @@
+@extends('adminlte::page')
+
+@section('title', 'Planos')
+
+@section('content_header')
+<ol class="breadcrumb my-2">
+    <li class="breadcrumb-item"><a href=" {{route('admin.index')  }} ">Dasboard</a></li>
+    <li class="breadcrumb-item active"><a href=" {{route('plans.index')  }} ">Planos</a></li>
+</ol>
+
+<h1>Planos <a href="{{ route('plans.create')}}" class="btn btn-sm btn-outline-primary ">
+        <i class="fas fa-plus"></i>
+        Add</a></h1>
+
+@stop
+
+
+@section('content')
+<p>Listagem dos planos</p>
+<div class="card">
+    <div class="card-header">
+        <form action="{{ route('plans.search') }}" method="POST" class="form form-inline">
+            @csrf
+            <div class="form-group">
+                <input type="text" name="filter" placeholder="Pesquisa" value="{{$filters['filter'] ?? ''}}"
+                    class="form-control">
+                <button type="submit" class="btn btn-outline-dark mx-2"> <i class="fas fa-search"></i> Filtrar</button>
+                <button type="button" class="btn  mx-2">Limpar Filtros</button>
+            </div>
+        </form>
+    </div>
+    <div class="card-body">
+        <table class="table table condensed">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Preço</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($plans as $plan)
+                <tr>
+                    <td>{{$plan->name}}</td>
+                    <td>R$ {{number_format($plan->price, 2, ',', '.')}}</td>
+                    <td>
+                        <a href="{{route('plans.show',$plan->url)}}" target="_blank"
+                            class="btn btn-sm btn-success">Ver</a>
+                    </td>
+                    <td>
+                        <a href="{{route('plan.edit',$plan->url)}}" target="_blank"
+                            class="btn btn-sm btn-warning">Editar</a>
+
+                    </td>
+                    <td>
+                        <form action="{{ route('plans.destroy', $plan->url)}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Deletar</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <div class="card-footer">
+        @if (isset($filters))
+        {!! $plans->appends($filters)->links() !!}
+        @else
+        {!! $plans->links() !!}
+        @endif
+    </div>
+</div>
+@stop
---
+++ resources/views/admin/pages/plans/show.blade.php
@@ -0,0 +1,26 @@
+@extends('adminlte::page')
+
+@section('title', 'Detalhes do Plano')
+
+@section('content_header')
+<h1> Detalhes do Plano</h1>
+@stop
+
+@section('content')
+
+<div class="card">
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item"> <strong>Nome:</strong> {{$plan->name}}</li>
+            <li class="list-group-item"> <strong>URL:</strong> {{$plan->url}}</li>
+            <li class="list-group-item"> <strong>Preço:</strong> R$ {{number_format($plan->price, 2, ',', '.')}}</li>
+            <li class="list-group-item"> <strong>Descrição:</strong> {{$plan->description}}</li>
+            <form action="{{ route('plans.destroy', $plan->url)}}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">Deletar</button>
+            </form>
+        </ul>
+    </div>
+</div>
+@stop
